6af86d8dc37ea0e1ecec36446e275b852de31c54
Ahmed Aldesoky <ahmedaldesoky134@gmail.com>
2022-11-01 19:59:12 +0200

    last

--- src/app/component/product/product.component.ts
+++ src/app/component/product/product.component.ts
@@ -176,10 +176,10 @@ filterCars(){
     return this.cars.filter((product) => 
     { 
       return(
-        product.maker.toLowerCase().indexOf( this.searchValue.maker?.toLowerCase())!=-1 ||
-        product.model.toLowerCase().indexOf( this.searchValue.model?.toLowerCase())!=-1 &&
-        product.location.toLowerCase().indexOf( this.searchValue.location?.toLowerCase())!=-1 &&
-        product.year.toString().indexOf( this.searchValue.year?.toLowerCase())!=-1 
+        product.maker.toLowerCase().indexOf( this.searchValue.maker?.toLowerCase())!==-1 ||
+        product.model.toLowerCase().indexOf( this.searchValue.model?.toLowerCase())!==-1 &&
+        product.location.toLowerCase().indexOf( this.searchValue.location?.toLowerCase())!==-1 &&
+        product.year.toString().indexOf( this.searchValue.year?.toLowerCase())!==-1 
         )}
         )
        }
@@ -193,7 +193,7 @@ filterProjectByLogo(filterTerm: string ){
  
     return this.cars.filter((product) => 
     { 
-      return product.maker.toLowerCase().indexOf( filterTerm?.toLowerCase())!=-1
+      return product.maker.toLowerCase().indexOf( filterTerm?.toLowerCase())!==-1
     })}
     submitData(){
       console.log(this.searchValue);
